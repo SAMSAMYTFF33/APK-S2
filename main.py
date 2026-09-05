eeeabeebe615d237776a8bb9f786db22cc39de9c
SAMSAMYTFF33 <miricanmoroco@gmail.com>
2026-09-05 14:54:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,12 +14,11 @@ from telethon.tl.types import KeyboardButtonWebView, KeyboardButtonSimpleWebView
 # =============================================================================
 # ⚙️ مفاتيح التحكم بحسابات ATF (1 = يعمل | 0 = متوقف)
 # ==============================================================================
-ATF_ACCOUNT_1   = 1     # ATF - gz (الحساب الأول)
-ATF_ACCOUNT_2   = 1     # ATF - الحساب ousama 
-ATF_ACCOUNT_3   = 1     # ATF - SKATE (الحساب الثالث)
-ATF_ACCOUNT_4   = 1     # ATF - الحساب الرابع
-ATF_ACCOUNT_5   = 1     # ATF - ZAMASO (الحساب الخامس)
-
+ATF_ACCOUNT_1   = 1     # ATF - gz 
+ATF_ACCOUNT_2   = 1     # ATF -  ousama 
+ATF_ACCOUNT_3   = 0     # ATF - SKATE 
+ATF_ACCOUNT_4   = 1     # ATF - AWF 
+ATF_ACCOUNT_5   = 1     # ATF - ZAMASO 
 # ==============================================================================
 # ⚙️ مفاتيح التحكم بحسابات BODA (1 = يعمل | 0 = متوقف)
 # ==============================================================================
