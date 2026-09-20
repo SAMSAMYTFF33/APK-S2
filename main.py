ad8315a67f14aecdc1fca6b375fb3e48685eab68
SAMSAMYTFF33 <miricanmoroco@gmail.com>
2026-09-20 17:15:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ ACCOUNTS_CONFIG = [
     {
         "atf_enabled": ATF_ACCOUNT_1 == 1,
         "account_name": "الحساب الأول (gz)",
-        "do_boost": True,
+        "do_boost": false,
         "api_id": 38197378,
         "api_hash": "1efeb1db162150616801ae759799ca97",
         "session_string": "1BJWap1wBu3SWNB8JFOdcM2T6cVu0o4dv7iybgtIqrRmUZYzkmWRkmBjFbGaovA7tyqfsozceWzvd9SuhsKsW1a9cle_PXkM_THwP_65_PYfO9w3aHVUvN_sIcfbnyQHz4AaVJhCyNEbwaRaZjShJvpZscoU_JLc0xD0rvE5wGQjEHZJkmL4OLsqoxZn0DgKqRtjLFX6KeQZinHJQeaFQQTqMdelSWmtE3diSNAV3JETvf7X2Llfb4dhVYbOAcMxm3ZRhRtv5uE9RjmMkS2OHOA8Dmr1OYn_E1r-xup8d2FifOMmI8QHcAS0ucEUwtgf5fS9AxtrLOS-JimS6tTNiiPcc7jZzRUU=",
